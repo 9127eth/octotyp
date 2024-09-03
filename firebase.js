23e158bc8e5e3103458e370166e48a1a558f3a1b
Richard Waithe <richardm1@Richards-MacBook-Pro-2.local>
2024-09-02 21:54:19 -0400

    updating key security and changes to auth

--- firebase.js
+++ firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCQFUeA6yRa0BmX6xBSPK7RJvw5Hvp1W8A",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "octotyp.firebaseapp.com",
     projectId: "octotyp",
     storageBucket: "octotyp.appspot.com",
     messagingSenderId: "346116172699",
     appId: "1:346116172699:web:39e30db03db57db4d68f3e",
     measurementId: "G-YH37E0J002"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
