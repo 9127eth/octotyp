4b874f976bf575fa11cd3e3a4360dfda5bd5dccc
Richard Waithe <richardm1@Richards-MacBook-Pro-2.local>
2025-02-27 00:00:45 -0500

    new home page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    plugins: [react()],
     server: {
       open: true, // automatically open the browser on server start
     },
+    build: {
+      outDir: 'dist',
+      sourcemap: true
+    }
 });
